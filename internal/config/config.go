de15cffd99fd6c0d3185456e755ece5ac35a67d6
Volteal <wendy.a.knight@icloud.com>
2024-08-01 12:38:35 +0200

    changed the default poert in the config file.

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,6 +15,6 @@ func Set() {
 	godotenv.Load("./env")
 
 	flag.BoolVar(&Prod, "prod", false, "Production mode; hide all errors.")
-	flag.StringVar(&Port, "port", ":8080", "The desired port to lsiten on. Example: -port=\":8080\"")
+	flag.StringVar(&Port, "port", "8080", "The desired port to lsiten on. Example: -port=\"8080\"")
 	flag.Parse()
 }
