a19497e9ca2dfd93ce60e1f1f656c4242263b78b
Volteal <wendy.a.knight@icloud.com>
2024-08-02 12:09:56 +0200

    Worked out the bugs in the build script. It not creates a release based on git version tags and zips the resulting folder.

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,18 +28,31 @@ do
       output_name+='.exe'
   fi
 
-	cpyfldr=./releases/release-$GOOS-${GOARCH}/$version
+	cpyfldr=./releases/release-$GOOS-$GOARCH/$output_name
 
   mkdir -p $cpyfldr
 	cp -r ./public $cpyfldr
 	cp -r .env $cpyfldr
 
-  echo "Building releases/release-${os}-${GOARCH}/$version/$output_name..."
+  echo "Building releases/release-$GOOS-$GOARCH-$version/$output_name..."
   env GOOS=$GOOS GOARCH=$GOARCH go build \
   -ldflags "-X github.com/VoltealProductions/athenaeum.Version=$version" \
-  -o $cpyfldr/$output_name ./cmd/webserver/main.go
+  -o $cpyfldr/$output_name ./cmd/client/main.go
   if [ $? -ne 0 ]; then
       echo 'An error has occurred! Aborting.'
       exit 1
+  fi
+  echo "Creating zip archives..."
+  zname=$package_name-$version-$GOOS-$GOARCH.zip
+  if [ $os = "windows" ]; then
+      zip -r ./releases/release-$GOOS-$GOARCH/$zname ./releases/release-$GOOS-$GOARCH/$output_name
+      chmod a+x ./releases/release-$GOOS-$GOARCH/$zname
+      zip -sf ./releases/release-$GOOS-$GOARCH/$zname
+      rm -rf ./releases/release-$GOOS-$GOARCH/$output_name
+  else
+      zip -r ./releases/release-$GOOS-$GOARCH/$zname ./releases/release-$GOOS-$GOARCH/$output_name
+      chmod a+x ./releases/release-$GOOS-$GOARCH/$zname
+      zip -sf ./releases/release-$GOOS-$GOARCH/$zname
+      rm -rf ./releases/release-$GOOS-$GOARCH/$output_name
   fi
 done
